1386a8e0a2fba3c0fe58c4d80b71d511f93b6b47
Yize Liu <yl7401@nyu.edu>
2025-02-16 20:39:57 -0500

    remove duplicate components

--- components/logger.py
+++
@@ -1,8 +0,0 @@
-import logging
-
-class Logger:
-    def __init__(self):
-        logging.basicConfig(filename="operations.log", level=logging.INFO)
-
-    def log(self, message: str):
-        logging.info(message)
